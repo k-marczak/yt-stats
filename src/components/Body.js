6b776dde6ed1fdad5d0a6a8e49e31505fccfe5f0
k-marczak <k_marczak@outlook.com>
2021-03-08 08:00:40 +0100

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Form from './Form'
+
+class Body extends Component {
+    render(){
+        return(
+            <div className="w-full container mx-auto my-12">
+                <Form />
+            </div>
+        )
+    }
+}
+
+export default Body
